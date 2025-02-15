c191056798e01f9d792f8035f51cb578e3ce0b5f
Jawne3 <johnlowendavid@yahoo.ca>
2025-02-15 00:06:01 -0700

    added a setter method to meal

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const menu = {
-    
+    _meal: '',
+    _price: 0,
+    set meal (mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            return this._meal = mealToCheck;
+        }
+    }
 }
